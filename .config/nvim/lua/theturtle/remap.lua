dfa3303da15856f2edf82a63364a81132a15fc8c
Andrew Pham <phambuibinhan@gmail.com>
2023-04-01 13:22:13 +1100

    used different cursor type between normal and insert mode

--- .config/nvim/lua/theturtle/remap.lua
+++ .config/nvim/lua/theturtle/remap.lua
@@ -38,6 +38,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
+
