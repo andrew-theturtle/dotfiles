5facebf6e6eaeb519f564137b326628fedeb59db
Andrew Pham <phambuibinhan@gmail.com>
2023-04-04 17:33:34 +1000

    updated tree to auto open and file current buffer

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -2,5 +2,5 @@ require('nvim-tree').setup()
 local api = require('nvim-tree.api')
 
 vim.keymap.set("n", "<leader>pv", function()
-    api.tree.toggle()
+    api.tree.toggle({ find_file = true, focus = true, open = true, path = vim.fn.expand("%:p:h") })
 end)
