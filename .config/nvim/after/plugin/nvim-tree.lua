24a90a227c32ab339615f7ee34932bfac1cdcfb5
Andrew Pham <phambuibinhan@gmail.com>
2023-04-26 22:51:46 +1000

    feat: rename and add config for nvim-tree

---
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,20 @@
+require('nvim-tree').setup({
+    git = {
+        ignore = false,
+    },
+    renderer = {
+        highlight_opened_files = '1',
+        group_empty = true,
+        icons = {
+            show = {
+                folder_arrow = false,
+            },
+        },
+        indent_markers = {
+            enable = true,
+        }
+    },
+})
+
+vim.keymap.set("n", "<leader>pv", ':NvimTreeFindFileToggle<CR>')
+
