48dd22cd1a700777e7e3215eb4641b6f2f7b22c0
Andrew Pham <phambuibinhan@gmail.com>
2023-11-06 14:31:43 +1100

    Add various Vim settings.

--- .config/nvim/lua/theturtle/set.lua
+++ .config/nvim/lua/theturtle/set.lua
@@ -16,6 +16,24 @@ vim.o.mouse = 'a'
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
+-- complete the longest common match, and allow tabbing the results to fully
+-- complete them
+vim.o.wildmode = 'longest:full,full'
+
+-- Set title of window to show file name
+vim.o.title = true
+
+vim.o.spell = true
+
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- ask for confirmation instead of showing error
+vim.o.confirm = true
+
+-- persistent undo
+vim.o.undofile = true
+
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
