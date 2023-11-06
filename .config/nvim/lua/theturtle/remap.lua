e128a8e0f0796ffdcdbe7eb5f6fa1363424a051a
Andrew Pham <phambuibinhan@gmail.com>
2023-11-06 14:31:34 +1100

    feat: add vim key mappings

--- .config/nvim/lua/theturtle/remap.lua
+++ .config/nvim/lua/theturtle/remap.lua
@@ -4,6 +4,8 @@ vim.g.mapleader = " "
 -- using J and K to move a block of visual line
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", ":m .+1<CR>==")
+vim.keymap.set("n", "K", ":m .-2<CR>==")
 
 -- J, take the next line and append it to the end of 
 -- current line with a space
@@ -11,8 +13,18 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- half page jumping but keep the cursor in the middle
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- maintain the cursor position when yanking in visual mode
+vim.keymap.set("v", "y", "myy`y")
+
+-- Disable annoying command line type
+vim.keymap.set("n", "q:", ":q")
+
+-- Easy insertion of a trailing ; or , from insert mode
+vim.keymap.set("i", ";;", "<Esc>A;")
+vim.keymap.set("i", ",,", "<Esc>A,")
 
 -- allow search term to stay in the middle
 vim.keymap.set("n", "n", "nzzzv")
@@ -33,10 +45,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Quick fix nav
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
