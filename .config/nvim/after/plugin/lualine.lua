f8d3c6046ee8046e84bbf3fe69eda85a030adeb9
Andrew Pham <phambuibinhan@gmail.com>
2023-11-06 14:30:59 +1100

    feat: Add global status to lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -3,6 +3,7 @@ local lualine = require('lualine')
 lualine.setup({
     options = {
         icons_enabled = true,
+        globalstatus = true,
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
