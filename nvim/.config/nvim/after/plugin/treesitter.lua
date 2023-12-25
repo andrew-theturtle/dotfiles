f32789a695962d70965c4310348ad98b6dac5893
Andrew Pham <62633701+andrew-theturtle@users.noreply.github.com>
2023-12-25 22:45:19 +1100

    feat: add go treesitter parser

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "cpp", "lua", "vim", "query", "tsx", "typescript",
-  			"javascript", "python", "php", "make", "cmake", "bash"},
+  			"javascript", "python", "php", "make", "cmake", "bash", "go"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
