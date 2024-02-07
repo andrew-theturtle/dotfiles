e2748f78806a1492c0925a835bf6fb3262c7dfb4
Andrew Pham <62633701+andrew-theturtle@users.noreply.github.com>
2024-02-08 10:19:35 +1100

    fix: use Meta instead of Ctrl for harpoon mapping

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -24,11 +24,11 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-;>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<M-n>", function() harpoon:list():next() end)
