592e6820c8142473eae202507f42c6357e6db5f9
Andrew Pham <62633701+andrew-theturtle@users.noreply.github.com>
2026-03-12 18:44:54 +1100

    chore: upgrade nvim telescope to v0.2.1

--- nvim/.config/nvim/lua/theturtle/packer.lua
+++ nvim/.config/nvim/lua/theturtle/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
+        'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
